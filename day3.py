2ed2cbe68c6def91ef68a34264ee58558e4bb418
psomepalli3 <35283824+psomepalli3@users.noreply.github.com>
2024-01-13 00:27:05 -0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,7 +1,7 @@
 ## Write a function to count the number of vowels in a given string 
 # Method 1: Using simple for loop
 def cnt_vowels(inp_str):
-  vowels = ['a', 'e', 'i', 'o', 'u']
+  vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
   cnt = 0
   for character in inp_str:
     cnt += sum(list(map(lambda vowel: vowel == character, vowels)))
@@ -9,7 +9,7 @@ def cnt_vowels(inp_str):
 
 # Method 2: Using lambda functions(map(), filter())
 def cnt_vowels1(inp_str):
-  vowels = ['a', 'e', 'i', 'o', 'u']
+  vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
   cnt = 0
   cnt = sum(list(map(lambda character: len(filter(lambda vowel: vowel == character, vowels)), inp_str)))
   return(cnt)
@@ -19,6 +19,6 @@ def cnt_vowels1(inp_str):
 print('Enter a string')
 inp_str = input()
 print('Method 1: Using simple for loop')
-print(cnt_vowels1(inp_str))
+print(cnt_vowels(inp_str))
 print('Method 2: Using lambda functions(map(), filter())')
 print(cnt_vowels1(inp_str))
