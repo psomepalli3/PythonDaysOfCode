ade130b187269ad8a5b4ba041610aec76f4c38a0
psomepalli3 <35283824+psomepalli3@users.noreply.github.com>
2024-01-13 01:45:24 -0500

    Update day3.py
    Function to count vowelsin a string

--- day3.py
+++ day3.py
@@ -8,11 +8,19 @@ def cnt_vowels(inp_str):
   return(cnt)
 
 # Method 2: Using lambda functions(map(), filter())
-def cnt_vowels1(inp_str):
+def cnt_vowels_map(inp_str):
   vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
   cnt = 0
   cnt = sum(list(map(lambda character: len(filter(lambda vowel: vowel == character, vowels)), inp_str)))
   return(cnt)
+
+# Method 3: Using filter function
+def cnt_vowels_filter(inp_str):
+    return len(list(filter(lambda x: x in "aeiouAEIOU", inp_str)))
+  
+# Method 4: Using list comprehension    ######## Found out to be the most efficient after profiled for time ######
+def cnt_vowels_list(inp_str):
+    return len([x for x in inp_str if x in "aeiouAEIOU"])
 
 
 print('Enter a string')
@@ -20,4 +28,8 @@ inp_str = input()
 print('Method 1: Using simple for loop')
 print(cnt_vowels(inp_str))
 print('Method 2: Using lambda functions(map(), filter())')
-print(cnt_vowels1(inp_str))
+print(cnt_vowels_map(inp_str))
+print('Method 3: Using filter function')
+print(cnt_vowels_filter(inp_str))
+print('Method 2: Using list comprehension')
+print(cnt_vowels_list(inp_str))
