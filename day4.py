d41b7a114652c42a90b9ca5e84c4b7520f678e1c
psomepalli3 <35283824+psomepalli3@users.noreply.github.com>
2024-01-15 20:36:47 -0500

    day4.py
    sum of all elements in a list

--- day4.py
+++ day4.py
@@ -1,4 +1,13 @@
 # Write a program to find the sum of all elements in a list.
-new_list= [5, 4, 6, 7, 8]
+def add(l):
+  s=0
+  for n in l:
+    s += n
+  return(s)
+
+
+new_list= [5, 4, -6, 7, 8, 0.33, 3]
+
 #sum of the all the elements
-print("sum of the all the elements", sum(new_list))
+print("sum of the all the elements using built in", sum(new_list))
+print("sum of the all the elements using funcion", add(new_list))
